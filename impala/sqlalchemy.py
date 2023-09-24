485c1658e3385860cf55b25654bf10dfe454823f
Richard Ellison <richard.ellison@optus.com.au>
2023-09-24 23:44:01 +1000

    Add column name adjustment based on pyhive

--- impala/sqlalchemy.py
+++ impala/sqlalchemy.py
@@ -180,6 +180,20 @@ class ImpalaExecutionContext(DefaultExecutionContext):
            cursor_configuration = self.execution_options.get('cursor_configuration', {})
            return self._dbapi_connection.cursor(configuration=cursor_configuration)
 
+       @util.memoized_property
+       def _preserve_raw_colnames(self):
+           # Ideally, this would also gate on hive.resultset.use.unique.column.names
+           return self.execution_options.get('hive_raw_colnames', False)
+
+       def _translate_colname(self, colname):
+           # Adjust for dotted column names.
+           # When hive.resultset.use.unique.column.names is true (the default), Hive returns column
+           # names as "tablename.colname" in cursor.description.
+           if not self._preserve_raw_colnames and '.' in colname:
+               return colname.split('.')[-1], colname
+           else:
+               return colname, None
+
 
 # Dialect to be used with Impala <= 3.4.0
 class ImpalaDialect(DefaultDialect):
